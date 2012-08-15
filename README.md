98fe2f0760a0bff7981ef18143d8a09bce8223b3
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-08-15 23:57:36 +0100

    Version 0
    Supports less, stylus, sass, coffee-script, markdown.  Needs tests.

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-build.js
-========
+consolidate-build
+=================
 
 Does for languages that can be 'built' what consolidate.js does for templates
+
+## Installation
+
+    $ npm install consolidate-build
+
+## Supported languages
+
+All template engines supported by [consolidate](https://github.com/visionmedia/consolidate.js) work in consolidate-build.
+
+In addition to those, consolidate build adds:
+
+ - [coffee-script](https://github.com/jashkenas/coffee-script) [(website)](http://coffeescript.org/)
+ - [less](https://github.com/cloudhead/less.js) [(website)](http://lesscss.org)
+ - markdown - will use [marked](https://github.com/chjj/marked), [discount](https://github.com/visionmedia/node-discount), [markdown-js](https://github.com/Gozala/markdown-js) or [markdown](https://github.com/evilstreak/markdown-js) depending on which one is installed (in that order of precedence if multiple markdown parsers are installed).
+ - [sass](https://github.com/visionmedia/sass.js) [(website)](http://sass-lang.com/)
+ - [stylus](https://github.com/learnboost/stylus) [(website)](http://learnboost.github.com/stylus/)
+
+__NOTE__: you must still install the engines you wish to use, add them to your package.json dependencies.
+
+## API
+
+All templates supported by this library may be rendered using the signature `(path[, locals], callback)` or `.render(str[, locals], callback)` as shown below.
+
+__NOTE__: All this example code uses build.less for the less stylsheet language. Replace less with whatever language you are using. For exmaple, use build.stylus for stylus, build.markdown or build.md for markdown, etc. `console.log(build)` for the full list of identifiers.
+
+```js
+var build = require('consolidate-build');
+build.less('styles/style.less', { compress: true }, function(err, css){
+  if (err) throw err;
+  console.log(css);
+});
+```
+
+Or without options / local variables:
+
+```js
+var build = require('consolidate-build');
+build.less('styles/style.less', function(err, css){
+  if (err) throw err;
+  console.log(css);
+});
+```
+
+To dynamically pass the engine, simply use the subscript operator and a variable:
+
+```js
+var build = require('consolidate-build')
+  , name = 'less';
+
+build[name]('styles/style.css', function(err, css){
+  if (err) throw err;
+  console.log(css);
+});
+```
+
+### Without a file
+
+All the languages support being built without a file being present.  To do this ismply use the render method.
+
+```js
+build.less('.class { width: 1 + 1 }', { compress: true}, function(err, css){
+  if (err) throw err;
+  console.log(css);
+});
+```
---
+++ index.js
@@ -0,0 +1,194 @@
+/**
+ * Module dependencies.
+ */
+
+var consolidate = require('consolidate');
+exports = module.exports = consolidate;
+var fs = require('fs');
+
+
+var readCache = {};
+
+/**
+ * Require cache.
+ */
+
+var cacheStore = {};
+
+/**
+ * Require cache.
+ */
+
+var requires = {};
+
+/**
+ * Clear the cache.
+ *
+ * @api public
+ */
+
+exports.clearCache = function(){
+  cacheStore = {};
+};
+
+function cache(options, compiled) {
+  if (compiled && options.filename && options.cache) {
+    delete readCache[options.filename];//don't need to cache in both locations
+    cacheStore[options.filename] = compiled;
+  } else if (options.filename && options.cache) {
+    return cacheStore[options.filename];
+  }
+  return compiled;
+}
+
+/**
+ * Read `path` with `options` with
+ * callback `(err, str)`. When `options.cache`
+ * is true the template string will be cached.
+ *
+ * @param {String} options
+ * @param {Function} fn
+ * @api private
+ */
+
+function read(path, options, fn) {
+  var str = readCache[path];
+
+  // cached (only if cached is a string and not a compiled template function)
+  if (options.cache && str && typeof str === 'string') return fn(null, str);
+
+  // read
+  fs.readFile(path, 'utf8', function(err, str){
+    if (err) return fn(err);
+    if (options.cache) readCache[path] = str;
+    fn(null, str);
+  });
+}
+
+/**
+ * fromStringRenderer
+ */
+
+function fromStringRenderer(name) {
+  return function(path, options, fn){
+    options.filename = path;
+    if (cache(options)) {
+      exports[name].render('', options, fn);//string doesn't matter if it's in the cache.
+    } else {
+      read(path, options, function(err, str){
+        if (err) return fn(err);
+        exports[name].render(str, options, fn);
+      });
+    }
+  };
+}
+
+
+/**
+ * options:
+ *  - paths: specifies search paths for @import directives
+ *  - filename: is required for @import directives and useful for error reporting
+ *  - compress: defaults to false, minifies the output if true.
+ */
+exports.less = fromStringRenderer('less');
+exports.less.render = function (str, options, fn) {
+    var engine = requires.less || (requires.less = require('less'));
+    var parser = new(less.Parser)(options || {});
+    parser.parse(str, function (e, tree) {
+        if (e) return fn(e);
+        var res;
+        try {
+            res = tree.toCSS(options);
+        } catch (ex) {
+            return fn(ex);
+        }
+        fn(null, res);
+    });
+};
+
+/**
+ * options:
+ *  - paths: specifies search paths for @import directives
+ *  - filename: is required for @import directives and useful for error reporting
+ *  - compress: defaults to false, minifies the output if true.
+ */
+exports.stylus = fromStringRenderer('stylus');
+exports.stylus.render = function (str, options, fn) {
+    var engine = require.stylus || (require.stylus = require('stylus'));
+    engine.render(str, options || {}, fn);
+};
+
+exports.sass = fromStringRenderer('sass');
+exports.sass.render = function (str, options, fn) {
+    var engine = require.sass || (require.sass = require('sass'));
+    var res;
+    try {
+        res = sass.render(str);
+    } catch (ex) {
+        return fn(ex);
+    }
+    fn(null, res);
+};
+
+exports.markdown = fromStringRenderer('md');
+exports.md = fromStringRenderer('md');
+exports.md.render = function (str, options, fn) {
+    var engine = require.markdown;
+
+    // support markdown / discount
+    if (!engine) {
+        var md;
+        try {
+            md = require('marked');
+            engine = function (str, options) {
+                return md.parse(str, options || {});
+            };
+        } catch (err){
+            try {
+                md = require('discount');
+                engine = function (str, options) {
+                    return md.parse(str);
+                };
+            } catch (err) {
+                try {
+                    md = require('markdown-js');
+                    engine = function (str, options) {
+                        return md.parse(str);
+                    };
+                } catch (err) {
+                    try {
+                        md = require('markdown');
+                        engine = function (str, options) {
+                            return md.parse(str);
+                        };
+                    } catch (err) {
+                        throw new
+                          Error('Cannot find markdown library, install markdown, discount, or marked.');
+                    }
+                }
+            }
+        }
+        require.markdown = engine;
+    }
+
+    var res;
+    try {
+        res = md(str, options);
+    } catch (ex) {
+        return fn(ex);
+    }
+    fn(null, res);
+};
+
+
+exports['coffee-script'] = exports.coffeescript = fromStringRenderer('coffeescript');
+exports.coffeescript.render = function (str, options, fn) {
+    var engine = require.coffeescript || (require.coffeescript = require('coffee-script'));
+    var res;
+    try {
+        res = engine.compile(str, options);
+    } catch (ex) {
+        return fn(ex);
+    }
+    fn(null, ex);
+};
