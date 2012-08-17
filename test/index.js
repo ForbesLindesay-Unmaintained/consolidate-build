5d3ad1f48d50d3fc31d17de9859f7b1e9c86d324
Forbes Lindesay <forbes.lindesay@red-gate.com>
2012-08-17 09:34:05 +0100

    Fix coffee script and add travis.yml

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
-//require('./shared').test('coffee', 'js', {});
+require('./shared').test('coffee', 'js', {});
 require('./shared').test('less', 'css', {});
 require('./shared').test('markdown', 'html', {});
-require('./shared').test('stylus', 'css', {});
 require('./shared').test('sass', 'css', {});
+require('./shared').test('stylus', 'css', {});
